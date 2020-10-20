6cc67f5fb1e86bc7e01141963750b673ff2244ba
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-10-21 01:09:19 +0900

    (BE) feat: 도서와 북마크 추가 쿼리 생성

--- server/models/queries.js
+++ server/models/queries.js
@@ -1,3 +1,4 @@
+const Book = require('./tables/book');
 const User = require('./tables/user');
 
 exports.findExistingUser = async (user_id, provider) => {
@@ -21,6 +22,24 @@ exports.findUserInfo = async (user_id, provider) => {
 exports.insertUser = async (user_id, user_name, password, provider) => {
   try {
     const result = await User.create({ user_id, user_name, password, provider });
+    return result;
+  } catch (e) {
+    return e;
+  }
+};
+
+exports.insertBook = async (title, authors, published_date, thumbnail_url) => {
+  try {
+    const result = await Book.create({ title, authors, published_date, thumbnail_url });
+    return result;
+  } catch (e) {
+    return e;
+  }
+};
+
+exports.insertBookmark = async (id, uid, book_id, text, highlights, omits, created) => {
+  try {
+    const result = await Book.create({ id, uid, book_id, text, highlights, omits, created });
     return result;
   } catch (e) {
     return e;
