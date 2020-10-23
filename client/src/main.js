028cde6df9d29d4e959eed6826ec491a710590ef
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-10-23 15:59:12 +0900

    (FE) feat: axios 기본 설정 - base URL 설정(api 서버) - 매 요청에 토큰을 담아서 보냄 - created hook에 로그인한 유저인지 확인하는 과정 추가

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import axios from 'axios';
 import { store } from './store';
 import BootstrapVue from 'bootstrap-vue';
 import '@/style/common.css';
@@ -10,10 +11,22 @@ import 'bootstrap-vue/dist/bootstrap-vue.css';
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
+axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.headers.common['Authorization'] = `Bearer ${store.state.userToken || ''}`;
+
 new Vue({
   store,
   router,
   components: { App },
   template: '<App/>',
+  created() {
+    axios
+      .get('/auth/is-auth')
+      .then((res) => {
+        if (res.data.authorized) store.state.authorized = true;
+        else store.state.authorized = false;
+      })
+      .catch((e) => alert('오류가 발생했습니다'));
+  },
   render: (h) => h(App),
 }).$mount('#app');
