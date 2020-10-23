9de7e9135b8dad1d188647f250cb891233a3d942
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-10-23 12:28:47 +0900

    (FE) refactor: vue-cli 적용을 위한 render - runtime compile을 위해 root component에 render 추가

--- client/src/main.js
+++ client/src/main.js
@@ -11,9 +11,9 @@ Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 new Vue({
-  el: '#app',
   store,
   router,
   components: { App },
   template: '<App/>',
-});
+  render: (h) => h(App),
+}).$mount('#app');
