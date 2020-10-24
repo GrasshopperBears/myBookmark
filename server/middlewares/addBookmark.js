d664619d97cc34a44c7f5dbc83a8a718b7cef5ec
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-10-24 17:16:44 +0900

    (BE) feat: 북마크 추가 미들웨어 작성

---
+++ server/middlewares/addBookmark.js
@@ -0,0 +1,25 @@
+const { insertBookmark } = require('../models/queries');
+
+const addBookmark = async (req, res, next) => {
+  if (!req.body.ok) return next();
+  try {
+    const result = await insertBookmark(
+      req.body.uid,
+      req.body.isbn,
+      req.body.text,
+      req.body.highlights,
+      req.body.omits,
+      new Date(),
+      req.body.page
+    );
+    req.body.ok = true;
+    req.body.result = result;
+    next();
+  } catch (e) {
+    req.body.ok = false;
+    req.body.message = '책갈피 추가 중 오류가 발생했습니다.';
+    next();
+  }
+};
+
+module.exports = addBookmark;
